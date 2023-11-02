55bc541b0f6cd510fa66ee40284c2c8754b3084f
dooraim <dooraim@yahoo.com>
2023-11-02 13:39:49 +0100

    Design Pattern: add Chain of responsability

--- Design_Pattern/Chain_of_Responsibility.c
+++ Design_Pattern/Chain_of_Responsibility.c
@@ -1,15 +1,25 @@
+/* Il design pattern "Chain of Responsibility" è utile quando hai una serie 
+di oggetti che devono gestire una richiesta in sequenza e ognuno di essi 
+ha la possibilità di gestire o trasmettere la richiesta al successivo nella catena.  */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+enum LevelLog{
+    CONSOLE,
+    FILE_L,
+    EMAIL
+};
+
 // Definizione di una struttura per rappresentare una richiesta di log
 typedef struct {
-    int level;
+    enum LevelLog level;
     char message[256];
 } LogRequest;
 
 // Definizione di una struttura per rappresentare un gestore di log
 typedef struct Logger {
-    int level;
+    enum LevelLog level;
     void (*log_message)(struct Logger *self, LogRequest *request);
     struct Logger *next;
 } Logger;
@@ -18,8 +28,7 @@ typedef struct Logger {
 void console_log(Logger *self, LogRequest *request) {
     if (request->level <= self->level) {
         printf("Log in console: %s\n", request->message);
-    }
-    if (self->next != NULL) {
+    } else if (self->next != NULL) {
         self->next->log_message(self->next, request);
     }
 }
@@ -31,8 +40,7 @@ void file_log(Logger *self, LogRequest *request) {
             fprintf(file, "Log in file: %s\n", request->message);
             fclose(file);
         }
-    }
-    if (self->next != NULL) {
+    } else if (self->next != NULL) {
         self->next->log_message(self->next, request);
     }
 }
@@ -40,8 +48,7 @@ void file_log(Logger *self, LogRequest *request) {
 void email_log(Logger *self, LogRequest *request) {
     if (request->level <= self->level) {
         printf("Invio email con log: %s\n", request->message);
-    }
-    if (self->next != NULL) {
+    } else if (self->next != NULL) {
         self->next->log_message(self->next, request);
     }
 }
@@ -49,17 +56,18 @@ void email_log(Logger *self, LogRequest *request) {
 int main() {
     // Creazione di gestori di log
     // Livello, tipo di log, next
-    Logger console = {1, console_log, NULL};
-    Logger file = {2, file_log, NULL};
-    Logger email = {3, email_log, NULL};
+    Logger console = {CONSOLE, console_log, NULL};
+    Logger file = {FILE_L, file_log, NULL};
+    Logger email = {EMAIL, email_log, NULL};
 
     // Collegamento dei gestori in una catena
     console.next = &file;
     file.next = &email;
 
     // Creazione di richieste di log
-    LogRequest request1 = {2, "Messaggio di log importante"};
-    LogRequest request2 = {3, "Messaggio di log critico"};
+    // definisci il livello che deve avere la richiesta e poi il messaggio della richiesta
+    LogRequest request1 = {FILE_L, "Messaggio di log importante"};
+    LogRequest request2 = {EMAIL, "Messaggio di log critico"};
 
     // Utilizzo della catena per gestire le richieste di log
     console.log_message(&console, &request1);  // Log in console e file
