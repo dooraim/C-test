62fbdd7b7b0174743a2f5f58302195be183d11cd
dooraim <dooraim@yahoo.com>
2023-11-10 22:20:32 +0100

    Pointers: add some function

---
+++ Pointers/Pointers_Arrays_2.c
@@ -0,0 +1,18 @@
+// C Program to access array elements using pointers
+#include <stdio.h>
+
+int main()
+{
+
+	// defining array
+	int arr[5] = { 1, 2, 3, 4, 5 };
+
+	// defining the pointer to array
+	int* ptr_arr = &arr[0];
+
+	// traversing array using pointer arithmetic
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", *ptr_arr++);
+	}
+	return 0;
+}
